b43c574f6cf93e1730c5564a405dbbfaf39da987
hs.code <foccusstyle@gmail.com>
2021-05-10 00:03:13 +0300

    chore: css - done

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-	<v-row justify="center" align="center">
-		<v-col cols="12" sm="8" md="6">
-			<div class="text-center">11</div>
+	<v-row class="keyboard-page" justify="center" align="center">
+		<v-col cols="12" sm="12" md="12">
+			<label class="literal-container"
+				><span class="literal success"> R</span><span class="literal"> W</span
+				><span class="literal fail"> [</span><span class="literal"> /</span
+				><span class="literal"> E</span><span class="literal"> W</span></label
+			>
+			<input type="text" class="input-area" />
+
+			<v-row class="info-container">
+				<v-col align="center">
+					<div class="number">38</div>
+					<div class="label">count</div>
+				</v-col>
+
+				<v-col align="center">
+					<div class="number">132</div>
+					<div class="label">speed</div>
+				</v-col>
+			</v-row>
 		</v-col>
 	</v-row>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {};
+	},
+	created() {},
+};
 </script>
